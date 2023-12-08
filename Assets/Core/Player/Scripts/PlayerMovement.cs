95b3049822a324712af2082ca1da1da49839c381
Louis <>
2023-12-08 10:05:57 +0100

    Add comment for understandability

--- Assets/Core/Player/Scripts/PlayerMovement.cs
+++ Assets/Core/Player/Scripts/PlayerMovement.cs
@@ -15,7 +15,11 @@ namespace Nano.Player
 
         private void FixedUpdate()
         {
+            //set rb velocity to move value. The reason we are using rigidbody is to allow players to push each others
             rb.velocity = new Vector3(player.playerData.CurrentInput.x, player.playerData.CurrentInput.y, 0) * moveSpeed;
+
+            //Dirty Safety measure : we make sure the player is at 0 in z position
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
         }
 
         public void OnNewMoveInput(Vector2 newMoveInput)
