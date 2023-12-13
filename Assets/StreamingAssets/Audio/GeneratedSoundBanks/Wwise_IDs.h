003a3c0496d0876b71dd44e0c35205b69ad640b3
Mattéo Blivet <blivet.pro@gmail.com>
2023-12-13 15:51:02 +0100

    Added Wwise events

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,44 +13,59 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID BGM_MAIN = 2633060887U;
         static const AkUniqueID BGM_TEST = 3438580564U;
+        static const AkUniqueID BULLETSHIELDABSORB_00_SFX = 1625499748U;
+        static const AkUniqueID COLLIDESHIELD_00_SFX = 1390461277U;
         static const AkUniqueID ENEMYNOTE1_00_SFX = 1961931039U;
         static const AkUniqueID ENEMYNOTE2_00_SFX = 686611850U;
         static const AkUniqueID ENEMYNOTE3_00_SFX = 3953467349U;
         static const AkUniqueID P1DAMAGEBIRD_00_SFX = 3391800229U;
+        static const AkUniqueID P1MAINDAMAGE_00_SFX = 1099326559U;
         static const AkUniqueID P1SHIELDBREAK1_00_SFX = 4183514192U;
         static const AkUniqueID P1SHIELDBREAK2_00_SFX = 95130393U;
         static const AkUniqueID P1SHIELDBREAK3_00_SFX = 2079705822U;
         static const AkUniqueID P1SHIELDGET1_00_SFX = 2750937717U;
         static const AkUniqueID P1SHIELDGET2_00_SFX = 2257478380U;
         static const AkUniqueID P1SHIELDGET3_00_SFX = 2849764031U;
+        static const AkUniqueID P1SUMMONFRIEND_00_SFX = 3115393712U;
         static const AkUniqueID P2DAMAGEBIRD_00_SFX = 911512040U;
+        static const AkUniqueID P2MAINDAMAGE_00_SFX = 2684841102U;
         static const AkUniqueID P2SHIELDBREAK1_00_SFX = 1676409973U;
         static const AkUniqueID P2SHIELDBREAK2_00_SFX = 1182950636U;
         static const AkUniqueID P2SHIELDBREAK3_00_SFX = 1775236287U;
         static const AkUniqueID P2SHIELDGET1_00_SFX = 1708052252U;
         static const AkUniqueID P2SHIELDGET2_00_SFX = 1711082405U;
         static const AkUniqueID P2SHIELDGET3_00_SFX = 3228990490U;
+        static const AkUniqueID P2SUMMONFRIEND_00_SFX = 4011626009U;
         static const AkUniqueID PAUSEBGM = 3403017773U;
         static const AkUniqueID RESUMEBGM = 3056597442U;
+        static const AkUniqueID SPECIALBULLET_00_SFX = 1500580661U;
         static const AkUniqueID UIMENUBACK_00_SFX = 207152438U;
         static const AkUniqueID UIMENUDOWN_00_SFX = 2305575365U;
         static const AkUniqueID UIMENUSELECT_00_SFX = 2622648053U;
         static const AkUniqueID UIMENUSLIDERTICKDOWN_00_SFX = 1415547993U;
         static const AkUniqueID UIMENUSLIDERTICKUP_00_SFX = 918622992U;
         static const AkUniqueID UIMENUUP_00_SFX = 2394293572U;
+        static const AkUniqueID UIP1CONNECTED_00_SFX = 144612902U;
+        static const AkUniqueID UIP1DISCONNECTED_00_SFX = 511490290U;
+        static const AkUniqueID UIP2CONNECTED_00_SFX = 2250873037U;
+        static const AkUniqueID UIP2DISCONNECTED_00_SFX = 878447239U;
         static const AkUniqueID WINGFLAP_00_SFX = 227607944U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
     {
         static const AkUniqueID ACTIONDUCK = 1722410376U;
+        static const AkUniqueID BGM_RTPC_SLIDER_VALUE = 36824089U;
         static const AkUniqueID DAMAGELOWCUT = 3953516820U;
         static const AkUniqueID DUCKNOTE1 = 1864835855U;
         static const AkUniqueID DUCKNOTE2 = 1864835852U;
         static const AkUniqueID DUCKNOTE3 = 1864835853U;
+        static const AkUniqueID MASTER_RTPC_SLIDER_VALUE = 2002689035U;
         static const AkUniqueID PAUSEBGMRTPC = 3969718864U;
         static const AkUniqueID PAUSEMUSICRTPC = 1925204343U;
+        static const AkUniqueID SFX_RTPC_SLIDER_VALUE = 623593826U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
